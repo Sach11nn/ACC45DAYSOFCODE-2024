e7d51db723975f3b4898234018b5b40eed74d854
Sachin Rajawat <sachinsinghrajawat9926@gmail.com>
2024-09-28 21:47:57 +0530

    Add files via upload

---
+++ Day 1- Find perimeter of square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+  int a,perimeter;
+  printf("enter the a:");
+  scanf("%d",&a);
+  perimeter=(4*a);
+  printf("the perimeter of square is:%d\n",perimeter);
+  return 0;
+}

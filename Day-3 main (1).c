ac6aa8af5cf5c3f58d8fe0e7fa6f0dfbbc63e3f8
Sachin Rajawat <sachinsinghrajawat9926@gmail.com>
2024-10-02 21:54:08 +0530

    Add files via upload

---
+++ Day-3 main (1).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int N,X;
+  
+    printf("Enter the lenght of the stick (N):");
+    scanf("%d",&N);
+    printf("Enter the desird lenght (X):");
+    scanf("%d",&X);
+    
+       { printf("Yes, chef can obtain a stick of lenght %d.\n",X);}
+    
+             {printf("No, chef cannot obtain a stick of lenght %d.\n",X);}
+    
+    return 0;
+    }

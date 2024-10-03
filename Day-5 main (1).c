7f71073e7a573bb64117256131bc05cb50ac0d5d
Sachin Rajawat <sachinsinghrajawat9926@gmail.com>
2024-10-03 22:27:38 +0530

    Add files via upload

---
+++ Day-5 main (1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    int B1,B2,B3;
+    printf("Enter the status of Bottle (0 = Empty,1 = Filled):");
+    scanf("%d %d %d",&B1,&B2,&B3);
+    
+    if(B1,B2,B3 ==0)
+        printf("Water filling time");
+    else
+    printf("Not now");
+  return 0;
+    }
+    
